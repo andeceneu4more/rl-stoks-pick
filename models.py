85faf71e77a554a45d3c9e213879c088100af512
Andrei Manea <andecenu@gmail.com>
2022-04-23 23:10:44 +0300

    separated into scripts and added a new improved trader

---
+++ models.py
@@ -0,0 +1,20 @@
+import torch.nn as nn
+
+class Estimator(nn.Module):
+    def __init__(self, state_size, action_space):
+        super().__init__()
+        self.state_size = state_size
+        self.action_space = action_space
+
+        self.model = nn.Sequential(
+            nn.Linear(state_size, 32),
+            nn.ReLU(),
+            nn.Linear(32, 64),
+            nn.ReLU(),
+            nn.Linear(64, 128),
+            nn.ReLU(),
+            nn.Linear(128, action_space)
+        )
+
+    def forward(self, x):
+        return self.model(x)
--- trader.py
+++ trader.py
@@ -1,112 +1,10 @@
-from collections import deque
 
 from tqdm import trange
 from pandas_datareader import data as data_reader
-from torch.optim import AdamW
+from trader_agents import ImprovedTrader, SimpleTrader
 
-import torch
-import torch.nn as nn
 import numpy as np
-import os
-import pdb
-
-
-class Estimator(nn.Module):
-    def __init__(self, state_size, action_space):
-        super().__init__()
-        self.state_size = state_size
-        self.action_space = action_space
-
-        self.model = nn.Sequential(
-            nn.Linear(state_size, 32),
-            nn.ReLU(),
-            nn.Linear(32, 64),
-            nn.ReLU(),
-            nn.Linear(64, 128),
-            nn.ReLU(),
-            nn.Linear(128, action_space)
-        )
-
-    def forward(self, x):
-        return self.model(x)
-
-
-class SimpleTrader():
-    def __init__(self, state_size, action_space=3, model_name="SimpleTrader"):
-        self.state_size = state_size
-        self.action_space = action_space
-        self.memory = deque()
-        self.portfolio = []
-        
-        self.model_name = model_name
-        self.model = Estimator(state_size, action_space)
-        self.optimizer = AdamW(self.model.parameters())
-        self.loss_fn = nn.MSELoss()
-        
-        self.gamma = 0.95
-        self.epsilon = 1.0
-        self.epsilon_final = 0.01
-        self.epsilon_decay = 0.995
-
-    def model_predict_proba(self, state):
-        state = torch.tensor(state).float()
-
-        self.model.eval()
-        with torch.no_grad():
-            logits = self.model(state)
-        return logits[0].numpy()
-
-    def model_predict(self, state):
-        logits = self.model_predict_proba(state)
-        return np.argmax(logits)
-
-    def save_model(self, exp_dir='logs/simple_trader'):
-        model_path = os.path.join(exp_dir, 'best.pth')
-        torch.save({
-            "model": self.model.state_dict()
-        }, model_path)
-
-    def training_step(self, state, target):
-        state = torch.tensor(state).float()
-        target = torch.tensor(target).float()
-
-        self.model.train()
-        self.optimizer.zero_grad()
-
-        logits = self.model(state)
-        loss = self.loss_fn(logits, target)
-
-        loss.backward()
-        self.optimizer.step()
-        
-
-    def trade(self, state):
-        rand = np.random.uniform(0, 1)
-        if rand <= self.epsilon:
-            return np.random.randint(low=0, high=self.action_space)
-
-        action = self.model_predict(state)
-        return action
-
-    def batch_train(self, batch_size):
-        batch = []
-        # TODO: check if iterates all of them
-        for i in range(len(self.memory) - batch_size + 1, len(self.memory)):
-            batch.append(self.memory[i])
-
-        for state, action, reward, next_state, done in batch:
-            if not done:
-                logits = self.model_predict_proba(next_state)
-                reward = reward + self.gamma * np.max(logits)
-
-            target = self.model_predict_proba(state)
-            target[action] = reward
-
-            target = np.array([target])            
-            self.training_step(state, target)
-
-        if self.epsilon > self.epsilon_final:
-            self.epsilon *= self.epsilon_decay
+# TODO: add device as well
 
 def sigmoid(x):
   return 1 / (1 + np.exp(-x))
@@ -135,8 +33,14 @@ n_episodes = 1000
 
 batch_size = 32
 data_samples = len(data) - 1
+sync_steps  = 1000
 
-trader = SimpleTrader(state_size=window_size)
+
+# TODO: put this in a function and make an inference for another set of yahoo stocks
+trader = ImprovedTrader(state_size=window_size)
+
+general_step = 0
+best_mean_reward = -1
 
 for episode in range(n_episodes):
   
@@ -145,6 +49,7 @@ for episode in range(n_episodes):
 
     total_profit = 0
     trader.portfolio = []
+    rewards = []    
   
     for t in trange(data_samples):
         action = trader.trade(state)
@@ -168,13 +73,19 @@ for episode in range(n_episodes):
         trader.memory.append((state, action, reward, next_state, done))
         state = next_state
 
+        rewards.append(reward)
+        mean_reward = np.mean(rewards)
+        if best_mean_reward < mean_reward:
+            best_mean_reward = mean_reward
+            trader.save_model()
+
+        general_step += 1
+        if general_step % sync_steps == 0:
+            trader.sync_target()
+
     print("#" * 20)
     print(f"Episode {episode} ended with a total profit = {total_profit}")
     print("#" * 20)
 
-    if len(trader.memory) > batch_size:
+    if len(trader.memory) > trader.replay_size:
         trader.batch_train(batch_size)
-        
-    if episode % 10 == 0:
-        trader.save_model()
-    
